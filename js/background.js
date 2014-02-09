de6212d8ac687648080bf4d7042c78d9dfe82650
Mathieu Gosbee <mail@matbee.com>
2014-02-09 17:39:33 +0000

    .

--- js/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-	// chrome.app.window.create('app.html', {
-	// 	'bounds': {
-	// 		'width': 400,
-	// 		'height': 500
-	// 	}
-	// });
-	window.open('app.html');
-});
--- makeln.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ln -T ../chrome-webserver/webserver.js ./js/webserver.js
-
